c09cfa80186f17e61cbc93a295476c1803fb7fdc
David Ruan <ruanwz@gmail.com>
2014-09-08 14:08:09 +0800

    docstring, doctest, async test

--- lib/mystreamers.ex
+++ lib/mystreamers.ex
@@ -1,16 +1,35 @@
 defmodule Mystreamers do
+  @doc """
+  find streaming file in given directory  
+
+  ## examples
+    iex> Mystreamers.find_index "dont_exists"
+    nil
+  """
   def find_index(dir) do
     files = Path.join(dir,"*.m3u8")
-    (Enum.find Path.wildcard(files), &is_index?(&1)) |> Path.basename
+    if file = Enum.find Path.wildcard(files), &is_index?(&1) do
+       Path.basename file
+    end
   end
 
-  def is_index?(file) do
+  defp is_index?(file) do
     # don't use exception for control, so File.read returns {:ok, "content"}
     # File.open! create a new process to manage the file, so return a pid
     File.open! file, [:read], fn(pid)->
+      # send msg to process to read file content
+      # that is why IO not block in Erlang, because it is concurrently
       IO.read(pid, 25) == "#EXTM3U\n#EXT-X-STREAM-INF"
     end
 
   end
 
 end
+    # everything in elixir like a keyword, need to use do block
+    # if is a macro, not a keyword
+    # if some do: xxx else: yyy is the same as :
+    # if some do
+    #   xxx
+    # else
+    #   yyy
+    # end
--- test/mystreamers_test.exs
+++ test/mystreamers_test.exs
@@ -1,10 +1,16 @@
 defmodule MystreamersTest do
-  use ExUnit.Case
+  # aync testing
+  use ExUnit.Case, async: true
+
+  doctest Mystreamers
 
   test "the truth" do
     assert 1 + 1 == 2
   end
   test "find index file" do
     assert Mystreamers.find_index("test/fixtures/emberjs") == "9af0270acb795f9dcafb5c51b1907628.m3u8"
+  end
+  test "return nil for non available index file" do
+    assert Mystreamers.find_index("test/fixtures/nondir") == nil
   end
 end
