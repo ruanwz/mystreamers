f1e561b7a5008822c958c9726eb0602f5e22bed5
David Ruan <ruanwz@gmail.com>
2014-09-09 11:13:20 +0800

    text processing

--- lib/mystreamers.ex
+++ lib/mystreamers.ex
@@ -1,6 +1,6 @@
 defmodule Mystreamers do
   require Record
-  Record.defrecord :m3u8, program_id: nil, path: nil, bandwidth: nil
+  Record.defrecord :m3u8, program_id: nil, path: nil, bandwidth: nil, ts_files: []
   @doc """
   find streaming file in given directory  
 
@@ -14,34 +14,72 @@ defmodule Mystreamers do
        file
     end
   end
+  @doc """
+  extract m3u8 records from index file
+  """
 
   def extract_m3u8(index_file) do
     # dont read all content into memory, open file in a process
     File.open! index_file, [:read], fn(pid)->
       IO.read(pid,:line)
       # use accumulator
-      do_extract_m3u8(pid, [])
+      do_extract_m3u8(pid, Path.dirname(index_file), [])
     end
 
   end
-  defp do_extract_m3u8(pid, acc) do
+  defp do_extract_m3u8(pid, dir, acc) do
     case IO.read(pid, :line) do
       :eof ->Enum.reverse acc
       stream_inf ->
         path = IO.read(pid, :line)
-        do_extract_m3u8(pid, stream_inf, path, acc)
+        do_extract_m3u8(pid, dir, stream_inf, path, acc)
     end
   end
-  defp do_extract_m3u8(pid, stream_inf, path, acc) do
+  defp do_extract_m3u8(pid, dir, stream_inf, path, acc) do
     # string in elixir is binary
     # <<"jos",x::utf8>> = "jos中"
     "#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=110000"
     << "#EXT-X-STREAM-INF:PROGRAM-ID=",program_id,",BANDWIDTH=",bandwidth::binary >> = stream_inf
 
+    path = Path.join(dir, String.strip(path))
     record = m3u8(program_id: program_id - ?0, path: String.strip(path), bandwidth: (bandwidth |> String.strip |> String.to_integer))
     acc = [record|acc]
-    do_extract_m3u8(pid, acc)
+    do_extract_m3u8(pid, dir, acc)
 
+  end
+  @doc """
+  process m3u8 records to get ts_files
+  """
+  def process_m3u8(m3u8s) do
+    Enum.map m3u8s, &do_process_m3u8(&1, self)
+
+  end
+
+  # do pattern matching in params
+  defp do_process_m3u8(m3u8(path: path)=m3u8_rec, parent_pid) do
+    #   #EXTM3U
+    #   #EXT-X-TARGETDURATION:11
+    #   #EXTINF:10,
+    #   265c58c98c2d8b04f21ea9d7b73ee4af-00001.ts
+    File.open! path, [:read], fn(pid)->
+      #skip line1
+      IO.read(pid,:line)
+      #skip line2
+      IO.read(pid,:line)
+      ts_files = do_process_m3u8(pid,[])
+      m3u8(m3u8, path: path, ts_files: ts_files)
+    end
+  end
+  defp do_process_m3u8(pid, acc) do
+    #skip
+    case IO.read(pid, :line) do
+      "#EXT-X-ENDLIST\n" ->Enum.reverse acc
+      _ext_info -> # skip first line
+        file = IO.read(pid, :line) |> String.strip
+        do_process_m3u8(pid,[file|acc])
+
+
+    end
   end
 
   defp is_index?(file) do
--- test/mystreamers_test.exs
+++ test/mystreamers_test.exs
@@ -5,6 +5,7 @@ defmodule MystreamersTest do
   doctest Mystreamers
 
   @index_file "test/fixtures/emberjs/9af0270acb795f9dcafb5c51b1907628.m3u8"
+  @sample_file "test/fixtures/emberjs/8bda35243c7c0a7fc69ebe1383c6464c.m3u8"
 
   test "the truth" do
     assert 1 + 1 == 2
@@ -17,9 +18,14 @@ defmodule MystreamersTest do
   end
   test "extract m3u8 info from index file" do
     m3u8s = Mystreamers.extract_m3u8 @index_file
-    assert Enum.at(m3u8s, 0) == (Mystreamers.m3u8(program_id: 1, bandwidth: 110000, path: "8bda35243c7c0a7fc69ebe1383c6464c.m3u8"))
+    assert Enum.at(m3u8s, 0) == (Mystreamers.m3u8(program_id: 1, bandwidth: 110000, path: @sample_file))
 
   end
 
+  test "process m3u8" do
+    m3u8s = @index_file |> Mystreamers.extract_m3u8 |> Mystreamers.process_m3u8
+    sample = Enum.find(m3u8s, fn(m3u8) -> Mystreamers.m3u8(m3u8,:path) == @sample_file end)
+    assert length(Mystreamers.m3u8(sample, :ts_files)) == 510
+  end
 
 end
